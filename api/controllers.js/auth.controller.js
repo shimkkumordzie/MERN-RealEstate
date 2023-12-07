1e0b909885d38f82fd41f6cb75ec57ddd279f781
shimkkumordzie <49838528+shimkkumordzie@users.noreply.github.com>
2023-12-07 17:24:47 +0000

    create singin api route

--- api/controllers.js/auth.controller.js
+++ api/controllers.js/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../modules/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +13,24 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfuly");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorhandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorhandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...restInfo } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(restInfo);
   } catch (error) {
     next(error);
   }
